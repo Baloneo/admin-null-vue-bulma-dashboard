882faf68e5b1968f1432b1c556dd08ad363b706e
Viktor Kuzhelny <viktor@marketto.ru>
2020-04-13 19:52:27 +0500

    Updates

---
+++ precache-manifest.1b17d3437d02833d70c4c6320caa4a44.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197e6cd8d1ce1c4886e8",
+    "url": "/css/app.4285499b.css"
+  },
+  {
+    "revision": "2597cec384cd22a97204",
+    "url": "/css/chunk-vendors.6a5e6c3f.css"
+  },
+  {
+    "revision": "eeaeb4a3eebc670da20483c2c01ac750",
+    "url": "/data-sources/clients.json"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/fonts/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "/img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "3c7b5bb832780ad9eb7dfe5e47e5d111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "197e6cd8d1ce1c4886e8",
+    "url": "/js/app.2bfc6fe9.js"
+  },
+  {
+    "revision": "2597cec384cd22a97204",
+    "url": "/js/chunk-vendors.ab8b9078.js"
+  },
+  {
+    "revision": "50c2cbd1138cf44c3038",
+    "url": "/js/client-form.9bbceba4.js"
+  },
+  {
+    "revision": "a453623202625e4ec5b5",
+    "url": "/js/client-form~forms.392212d2.js"
+  },
+  {
+    "revision": "88b20fa400bbdb609814",
+    "url": "/js/forms.8f6c229c.js"
+  },
+  {
+    "revision": "10b7f93a573b1c0bde33",
+    "url": "/js/profile.abdb5622.js"
+  },
+  {
+    "revision": "d0f9db705583050b9763",
+    "url": "/js/tables.32069f54.js"
+  },
+  {
+    "revision": "9e02268cb14385af9653507bc8b8ee72",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
