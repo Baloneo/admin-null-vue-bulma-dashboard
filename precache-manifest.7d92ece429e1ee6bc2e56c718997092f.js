b80a9f8e41b1dedcfd24adca8343dfab17944f91
Viktor Kuzhelny <viktor@marketto.ru>
2020-06-10 13:46:58 +0500

    Updates

---
+++ precache-manifest.7d92ece429e1ee6bc2e56c718997092f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630dfc25525bd5b080ec",
+    "url": "/admin-null-vue-bulma-dashboard/css/app.1bcfd85b.css"
+  },
+  {
+    "revision": "eeaeb4a3eebc670da20483c2c01ac750",
+    "url": "/admin-null-vue-bulma-dashboard/data-sources/clients.json"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/admin-null-vue-bulma-dashboard/favicon.png"
+  },
+  {
+    "revision": "4ab61b2e512db404026b048d56ae2881",
+    "url": "/admin-null-vue-bulma-dashboard/index.html"
+  },
+  {
+    "revision": "630dfc25525bd5b080ec",
+    "url": "/admin-null-vue-bulma-dashboard/js/app.e126aae0.js"
+  },
+  {
+    "revision": "6f6721cb57d531522d6c",
+    "url": "/admin-null-vue-bulma-dashboard/js/chunk-vendors.6c50ea45.js"
+  },
+  {
+    "revision": "2fb2f91a536934cea3a9",
+    "url": "/admin-null-vue-bulma-dashboard/js/client-form.72ddadb3.js"
+  },
+  {
+    "revision": "fd557abb9183af262be7",
+    "url": "/admin-null-vue-bulma-dashboard/js/client-form~forms.59cd9ddb.js"
+  },
+  {
+    "revision": "e6038b67eb53b43363fc",
+    "url": "/admin-null-vue-bulma-dashboard/js/forms.d3dfee7d.js"
+  },
+  {
+    "revision": "2dedb3c4df0545eeccd5",
+    "url": "/admin-null-vue-bulma-dashboard/js/profile.84429ed9.js"
+  },
+  {
+    "revision": "548e20e6f70cd0e34e9b",
+    "url": "/admin-null-vue-bulma-dashboard/js/tables.a244e069.js"
+  },
+  {
+    "revision": "9e02268cb14385af9653507bc8b8ee72",
+    "url": "/admin-null-vue-bulma-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin-null-vue-bulma-dashboard/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/admin-null-vue-bulma-dashboard/precache-manifest.638133303291c512a5885e990270d76f.js"
+  "/admin-null-vue-bulma-dashboard/precache-manifest.7d92ece429e1ee6bc2e56c718997092f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "admin-null-vue-bulma-dashboard"});
